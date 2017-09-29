5caa1d9ba7ee90d5e2fa2a1cb8e819768f22505c
Cillié Malan <cilliemalan@gmail.com>
2017-09-29 11:29:27 +0200

    made version string look a bit better

--- app/include/user_version.h
+++ app/include/user_version.h
@@ -6,9 +6,9 @@
 #define NODE_VERSION_REVISION	0U
 #define NODE_VERSION_INTERNAL   0U
 
-#define NODE_VERSION	"NodeMCU 2.1.0 Rabbiteer Edition"
+#define NODE_VERSION	"NodeMCU Rabbiteer Edition"
 #ifndef BUILD_DATE
-#define BUILD_DATE	  __DATE__
+#define BUILD_DATE	  __DATE__ " " __TIME__
 #endif
 
 extern char SDK_VERSION[];
